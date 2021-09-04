d0fe115d7fa0b6ea26d13792a4f832ea017fb164
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-09-04 18:18:21 +0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Redirect, Route, Switch } from 'react-router';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/welcome" />
+                    </Route>
+                    <Route path="/welcome">
+                        <Welcome />
+                    </Route>
+                    <Route path="/products" exact>
+                        <Products />
+                    </Route>
+                    <Route path="/products/:productId">
+                        <ProductDetail />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
